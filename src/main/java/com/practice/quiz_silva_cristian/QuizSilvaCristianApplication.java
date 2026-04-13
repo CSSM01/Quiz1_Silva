131ae8d6bc27d3d91b618e715bd3bb44e95347c5
CSSM01 <cristiansantiagosilvamunoz@gmail.com>
2026-04-13 11:11:12 -0500

    Inicio Commits de Quiz de Desarrollo Empresarial; Cristian Silva

---
+++ src/main/java/com/practice/quiz_silva_cristian/QuizSilvaCristianApplication.java
@@ -0,0 +1,13 @@
+package com.practice.quiz_silva_cristian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizSilvaCristianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizSilvaCristianApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/quiz_silva_cristian/QuizSilvaCristianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.quiz_silva_cristian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizSilvaCristianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
